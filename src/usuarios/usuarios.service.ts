1d84f0bd4f30009502c9d7c7d79033d5fc297584
alfredo215 <mc1121032018@unab.edu.sv>
2023-08-24 13:58:29 -0600

    segundo commit

--- src/usuarios/usuarios.service.ts
+++ src/usuarios/usuarios.service.ts
@@ -1,16 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
+import { UpdateUsuarioDto } from './dto/update-usuario.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUsuario } from './interface/create-usuario.interface';
-import { CreateUsuarioDto } from './dto/create-usuario.dto';
-import { UpdateUsuarioDto } from './dto/update-usuario.dto';
 import * as bcrypt from 'bcryptjs';
-
+import * as jwt from 'jsonwebtoken';
+import { LoginUsuario } from './interface/login-usuario.interface';
 @Injectable()
 export class UsuariosService {
 
   constructor(private readonly prismaService: PrismaService) {}
   async create({email, nombre, clave}:CreateUsuario) {
     const claveencriptada= await  bcrypt.hash(clave,10)
+
+    const usuarioExistente= await this.prismaService.usuarios.findUnique({
+      where:{
+        email
+      }
+    })
+
+    if(usuarioExistente){
+      throw new HttpException("El usuario ya existe, no puede registrar.",409);
+    }
+
+
     const usuario=await this.prismaService.usuarios.create({
       data:{
         email,
@@ -19,11 +31,45 @@ export class UsuariosService {
       },
     })
 
-
-    return usuario
+    const token= jwt.sign(
+      {email:usuario.email,nombre:usuario.nombre},
+      process.env.CLAVE_SECRETA,
+      {expiresIn:'36000s'}
+      )
+    
+    return token
   }
 
+  async login({email, clave}:LoginUsuario) {
+    const claveencriptada= await  bcrypt.hash(clave,10)
 
+    const usuarioExistente= await this.prismaService.usuarios.findUnique({
+      where:{
+        email
+      }
+    })
+
+    if(usuarioExistente){
+      //comprobar hash de la contraseña
+      const passwordCorrecto= await bcrypt.compare(clave,usuarioExistente.clave);
+      if(!passwordCorrecto){
+        throw new HttpException("La clave es incorrecta.",400);
+      }else{
+        const token= jwt.sign(
+          {email:usuarioExistente.email,nombre:usuarioExistente.nombre},
+          process.env.CLAVE_SECRETA,
+          {expiresIn:'36000s'}
+          )
+        
+        return token
+      }
+    }else{
+      throw new HttpException("El usuario no existe.",400);
+    }
+
+
+   
+  }
   findAll() {
     return `This action returns all usuarios`;
   }
